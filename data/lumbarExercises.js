97b6250f9164594ffc2849cb9d20ef8fc50c2de9
mylenegadoury-lab <mylene.gadoury@me.com>
2025-12-04 17:41:59 -0500

    Create lumbarExercises.js

---
+++ data/lumbarExercises.js
@@ -0,0 +1,56 @@
+export const lumbarExercises = [
+  {
+    id: "mckenzie-elbows",
+    name: "Extension en appui sur les coudes (McKenzie)",
+    tags: ["extension", "centralisation", "preference-extension"],
+    description: "Allongez-vous sur le ventre, placez vos coudes sous les épaules et laissez le bas du dos se relâcher.",
+    image: "/images/mckenzie-elbows.png",
+    video: "https://youtu.be/8QPBsq7Gk-g",
+    progression: "Extension bras tendus"
+  },
+  {
+    id: "cat-cow",
+    name: "Chat-vache",
+    tags: ["mobilité", "échauffement"],
+    description: "À quatre pattes, alternez arrondir et creuser le dos.",
+    image: "/images/cat-cow.png",
+    video: "https://youtu.be/kqnua4rHVVA",
+    progression: "Plus grande amplitude"
+  },
+  {
+    id: "dead-bug",
+    name: "Dead Bug",
+    tags: ["stabilité", "core", "instabilité"],
+    description: "Sur le dos, abaissez bras droit + jambe gauche sans cambrer.",
+    image: "/images/dead-bug.png",
+    video: "https://youtu.be/g_Ne0M2aG4I",
+    progression: "Dead bug avec poids"
+  },
+  {
+    id: "bridge",
+    name: "Pont fessier",
+    tags: ["renforcement", "chaîne-postérieure"],
+    description: "Soulevez le bassin en serrant les fessiers.",
+    image: "/images/bridge.png",
+    video: "https://youtu.be/m2Zx-1iGa0I",
+    progression: "Pont unilatéral"
+  },
+  {
+    id: "bird-dog",
+    name: "Bird-Dog",
+    tags: ["stabilité", "contrôle-moteur"],
+    description: "Allongez bras droit et jambe gauche sans cambrer.",
+    image: "/images/bird-dog.png",
+    video: "https://youtu.be/wUEl8KrMz14",
+    progression: "Bird-dog chargé"
+  },
+  {
+    id: "lumbar-rotation",
+    name: "Rotation lombaire couchée",
+    tags: ["mobilité", "rotation"],
+    description: "Laissez tomber les genoux d’un côté puis de l’autre.",
+    image: "/images/lumbar-rotation.png",
+    video: "https://youtu.be/TcZyZDTxgPI",
+    progression: "Maintien 10 secondes"
+  }
+];
